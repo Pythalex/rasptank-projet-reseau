fee1971b4d8b4e7ba30ed8328844ff6b667a7094
Pythalex <hyorvenn@protonmail.com>
2021-05-21 13:05:29 +0200

    client pc first version

---
+++ clientpc.py
@@ -0,0 +1,81 @@
+import socket
+import threading
+import sys
+import time
+from pynput.keyboard import Key, Listener
+
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 9000        # The port used by the server
+conn = None
+
+IN_BYTE_MAX_SIZE = 1024
+ROBOT_BUFFER_REFRESH_TIME = 10 #ms
+
+class Protocol:
+    forward = b"forward"
+    backward = b"backward"
+    left = b"left"
+    right = b"right"
+    speed = b"speed 100"
+    move_command = [forward, backward, left, right]
+
+    connect = "connect"
+
+key_binding = {
+    "up"    : Protocol.forward,
+    "down"  : Protocol.backward,
+    "left"  : Protocol.left,
+    "right" : Protocol.right,
+    "s"     : Protocol.speed
+}
+    
+
+def decode_byte_to_str(b):
+    try:
+        decoded = b.decode("utf-8")
+        return decoded
+    except:
+        print(f"[THREAD] Error when decoding input byte as utf-8: {b}")
+        return None
+
+
+def recv_wait(conn):
+    while True:
+        data = decode_byte_to_str(conn.recv(IN_BYTE_MAX_SIZE))
+        if not data:
+            time.sleep(ROBOT_BUFFER_REFRESH_TIME)
+        else:
+            return data
+
+
+def on_press(key):
+    try:
+        conn.sendall(key_binding[key.name])
+    except:
+        conn.sendall(key_binding[key.char])
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+def main():
+    global conn
+
+    robotname = input("Enter robot name to connect to : ")
+
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.connect((HOST, PORT))
+    conn.sendall(f"{Protocol.connect} {robotname}".encode())
+    data = conn.recv(IN_BYTE_MAX_SIZE).decode("utf-8")
+    print(f"Server response : {data}")
+
+    # Collect events until released
+    with Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+main()
